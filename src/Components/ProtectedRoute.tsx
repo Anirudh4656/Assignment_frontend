41f67373dccb6569ce6bfdf3b44458eb6780acce
ANIRUDH <anirudh4656@gmail.com>
2024-07-14 19:49:57 +0530

    changes

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -2,15 +2,14 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-//user state se pta krna ke h ya nhi
-//state mestore karwa sakta hu and aur treka?
+
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
-//id 
+//in case of true  
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  return true ? <>{children}</> : <Navigate to="/Plans" />;
+  return  <>{children}</> ;
 };
 
 export default ProtectedRoute;
