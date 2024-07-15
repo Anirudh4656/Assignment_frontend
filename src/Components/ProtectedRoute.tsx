1f0d57f4859e41c974b9955401fbd0c33d5f35f6
ANIRUDH <83304642+Anirudh4656@users.noreply.github.com>
2024-07-15 15:52:47 +0530

    Update ProtectedRoute.tsx

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,14 +1,23 @@
 // src/components/ProtectedRoute.tsx
+import { RootState } from "../Store/store";
 import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+
 
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
-//in case of true  
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const result = useSelector((state: RootState) => state.auth.user);
+
+  if (  result?.role !== "ADMIN") {
+    return <Navigate to="/" />; 
+  }
+
+console.log("user.role in protected route",result?.role)
   return  <>{children}</> ;
 };
 
